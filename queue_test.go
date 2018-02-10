3eb0ad90784c9a2dce9111647925d50f40fce2cb
Nayuta Yanagisawa <nayuta@yanagisawa.me>
2018-02-10 14:43:45 +0900

    Implement benchmark (random enq/deq)

--- queue_test.go
+++ queue_test.go
@@ -3,8 +3,11 @@ package pool
 import (
 	"fmt"
 	"sync"
+	"sync/atomic"
 	"testing"
 	"testing/quick"
+
+	"github.com/golang/go/src/pkg/math/rand"
 )
 
 func Example() {
@@ -50,6 +53,33 @@ func runSliceQueue(inputs []int) (outputs []interface{}) {
 func TestMatchWithSliceQueue(t *testing.T) {
 	if err := quick.CheckEqual(runQueue, runSliceQueue, nil); err != nil {
 		t.Error(err)
+	}
+}
+
+func Benchmark_Stack_Random(b *testing.B) {
+	length := 1 << 12
+	inputs := make([]int, length)
+	for i := 0; i < length; i++ {
+		inputs = append(inputs, rand.Int())
+	}
+	q, sq := NewQueue(), NewSliceQueue()
+	b.ResetTimer()
+
+	for _, q := range [...]queueInterface{q, sq} {
+		b.Run(fmt.Sprintf("%T", q), func(b *testing.B) {
+			var c int64
+			b.RunParallel(func(pb *testing.PB) {
+				for pb.Next() {
+					i := int(atomic.AddInt64(&c, 1)-1) % length
+					v := inputs[i]
+					if v >= 0 {
+						q.Enq(v)
+					} else {
+						q.Deq()
+					}
+				}
+			})
+		})
 	}
 }
 
