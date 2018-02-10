63ace81c69a8c55828069172e595685765f550d5
Nayuta Yanagisawa <nayuta@yanagisawa.me>
2018-02-10 14:42:06 +0900

    Implement black box test for Queue

--- queue_test.go
+++ queue_test.go
@@ -1,8 +1,57 @@
 package pool
 
 import (
+	"fmt"
 	"sync"
+	"testing"
+	"testing/quick"
 )
+
+func Example() {
+	q := NewQueue()
+
+	q.Enq("1st item")
+	q.Enq("2nd item")
+	q.Enq("3rd item")
+
+	fmt.Println(q.Deq())
+	fmt.Println(q.Deq())
+	fmt.Println(q.Deq())
+	// Output:
+	// 1st item
+	// 2nd item
+	// 3rd item
+}
+
+type queueInterface interface {
+	Enq(interface{})
+	Deq() interface{}
+}
+
+func run(inputs []int, q queueInterface) (outputs []interface{}) {
+	for _, v := range inputs {
+		if v >= 0 {
+			q.Enq(v)
+		} else {
+			outputs = append(outputs, q.Deq())
+		}
+	}
+	return outputs
+}
+
+func runQueue(inputs []int) (outputs []interface{}) {
+	return run(inputs, NewQueue())
+}
+
+func runSliceQueue(inputs []int) (outputs []interface{}) {
+	return run(inputs, NewSliceQueue())
+}
+
+func TestMatchWithSliceQueue(t *testing.T) {
+	if err := quick.CheckEqual(runQueue, runSliceQueue, nil); err != nil {
+		t.Error(err)
+	}
+}
 
 type SliceQueue struct {
 	s  []interface{}
